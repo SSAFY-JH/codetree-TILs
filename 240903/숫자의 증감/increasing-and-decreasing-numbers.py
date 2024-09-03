5ce6c022f14bcc7da5acd5c7f474b6af633f9b62
JH <cjh38510@naver.com>
2024-09-03 15:30:20 +0900

    Title: 숫자의 증감, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240903/숫자의 증감/increasing-and-decreasing-numbers.py
+++ 240903/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -4,6 +4,6 @@ n = int(n)
 if c == "A":
     for i in range(1, n+1):
         print(i, end = " ")
-elif c == "D":
-    for j in range(n, 1, -1):
+if c == "D":
+    for j in range(n, 0, -1):
         print(j, end = " ")
