e3edad9839b49dc19096f415ee7790447e065e8d
JH <cjh38510@naver.com>
2024-09-03 17:05:06 +0900

    Title: 369 게임, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240903/369 게임/369-game.py
+++ 240903/369 게임/369-game.py
@@ -1,7 +1,7 @@
 n = int(input())
 
 for i in range(1, n+1):
-    if i % 3 == 0:
+    if i % 3 == 0 or 3 in i or 6 in i or 9 in i:
         print(0, end = ' ')
     else:
         print(i, end = ' ')
