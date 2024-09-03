9fb838bdf7a5430eac240f92c52226d958c326be
JH <cjh38510@naver.com>
2024-09-03 09:17:42 +0900

    Title: 알파벳에 따른 평가, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240903/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+string = input()
+
+if string == "S":
+    print("Superior")
+elif string == "A":
+    print("Exellent")
+elif string == "B":
+    print("Good")
+elif string == "C":
+    print("Usually")
+elif string == "D":
+    print("Effort")
+else:
+    print("Failure")
