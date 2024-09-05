792cf7ce0088823e4cc6c388ca5db8685d4c1f80
JH <cjh38510@naver.com>
2024-09-05 13:37:08 +0900

    Title: 별 그리기, Time: 156ms, Memory: 26MB, Status: Passed - CodeTree

--- 240905/별 그리기/star-drawing.py
+++ 240905/별 그리기/star-drawing.py
@@ -10,6 +10,6 @@ for i in range(n):
 for i in range(n - 1):
     for j in range(i + 1):
         print(' ', end = "")
-    for j in range(n - (2 * i)):
+    for j in range((2 * n) - (2 * i) - 3):
         print('*', end = "")
     print()
