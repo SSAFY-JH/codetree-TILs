3bf31379409458d559a6b2b883d4aaa36a3b0b9c
JH <cjh38510@naver.com>
2024-09-05 10:07:15 +0900

    Title: 출석 부르기 2, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 240905/출석 부르기 2/calling-attendance-2.py
+++ 240905/출석 부르기 2/calling-attendance-2.py
@@ -1,13 +1,16 @@
 while True:
-    n = int(input())
-    if n == 1:
-        print("John")
-    elif n == 2:
-        print("Tom")
-    elif n == 3:
-        print("Paul")
-    elif n == 4:
-        print("Sam")
-    else:
-        print("vacancy")
-        break
+    # 변수 선언 및 입력
+	n = int(input())
+		
+	# n이 1~4라면 번호에 맞는 이름을, 그게 아니라면 Vacancy를 출력한 뒤 종료합니다.
+	if n == 1:
+		print("John")
+	elif n == 2:
+		print("Tom")
+	elif n == 3:
+		print("Paul")
+	elif n == 4:
+		print("Sam")
+	else:
+		print("Vacancy")
+		break
