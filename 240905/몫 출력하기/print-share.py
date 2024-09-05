101dab53188578b517adde04ed9cc91d4327a10f
JH <cjh38510@naver.com>
2024-09-05 10:10:50 +0900

    Title: 몫 출력하기, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240905/몫 출력하기/print-share.py
+++ 240905/몫 출력하기/print-share.py
@@ -1,6 +1,11 @@
+cnt = 0
+
 while True:
     n = int(input())
     if n % 2 == 1:
-        pass
-    elif n % 2 == 0:
-        print(n//2)
+        continue
+    print(n//2)
+    cnt += 1
+
+    if cnt >= 3:
+        break
