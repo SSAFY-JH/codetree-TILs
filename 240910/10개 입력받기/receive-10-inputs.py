f8d5ef4e8302d1b9fe2fa576cd3bee729d2d74fb
JH <cjh38510@naver.com>
2024-09-10 15:00:11 +0900

    Title: 10개 입력받기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240910/10개 입력받기/receive-10-inputs.py
@@ -0,0 +1,14 @@
+arr = list(map(int, input().split()))
+
+plus = 0
+cnt = 0
+
+for i in arr:
+    if i == 0:
+        break
+    plus += i
+    cnt += 1
+
+result = plus / cnt
+
+print(plus, result)
